b963c566fa39d96d1f765f066514b0b85b0b5b07
Fernando Chiquito <fernando.chiquito@icloud.com>
2020-09-12 07:56:25 -0300

    Starting to work on the javascrip for the site

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function person(name, favColor) {
+    console.log("Hello, my name is " + name + " and my favorite color is " + favColor + ".");
+}
+
+let johnName = "John Doe";
+let johnFavColor = "blue";
+
+person(johnName, johnFavColor)
+person("Jane Smith", "green")
